e2f113f6d75c25bb357cab3485c80afe450b8a7e
Frank Pfattheicher <fpf@ict-baden.de>
2016-10-17 21:57:25 +0200

    Event sender added.

---
+++ EventSender/EventSender/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace EventSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var eventObject = new JObject();
+            foreach(var arg in args)
+            {
+                var keyValue = arg.Split('=');
+                eventObject.Add(keyValue[0], keyValue[1]);
+            }
+            var json = JsonConvert.SerializeObject(eventObject);
+
+            Console.WriteLine(json);
+
+            var client = new UdpClient();
+            client.Connect("192.168.2.255", 22022);
+
+            var data = Encoding.UTF8.GetBytes(json);
+            client.Send(data, data.Length);
+        }
+    }
+}
